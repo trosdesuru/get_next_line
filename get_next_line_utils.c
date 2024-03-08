c419e7c66e568f8ee4219eed4500952486fb3ac3
Eduard Hernandez Ventos <edhernan@cbr6s5.42barcelona.com>
2024-03-08 19:38:17 +0100

    Update strjoin | version 1.8

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: edhernan <edhernan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 09:15:20 by edhernan          #+#    #+#             */
-/*   Updated: 2024/03/08 16:56:47 by edhernan         ###   ########.fr       */
+/*   Updated: 2024/03/08 19:37:48 by edhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,6 @@ int main (void) //Function string character(ft_strchr) TEST.
 
 	c = '\n';
 	str = ft_strchr("My \n name is \n Edu", '\n');
-	printf("Busca el caracter = %s\n"ft_strchr(str, c))
+	printf("Busca el caracter = %s\n"ft_strchr(str, c));
 	return (0);
 }
